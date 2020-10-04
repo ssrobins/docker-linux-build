ce42c38738d19968d62eccecae1b9350c561fe50
Steve Robinson <ssrobins@gmail.com>
2020-10-03 18:34:47 -0700

    Changes to get GitHub Actions going

--- build.sh
+++ build.sh
@@ -4,8 +4,7 @@ cd $(dirname "$0")
 
 gcc_version=9.3.0
 
-if [ -z "$CI_REGISTRY_IMAGE" ]; then
-    CI_REGISTRY_IMAGE=docker-linux-build
-fi
+CI_REGISTRY_IMAGE=ghcr.io/ssrobins/docker-linux-build
+
 
 docker build --pull --tag "$CI_REGISTRY_IMAGE:gcc$gcc_version" . --build-arg "gcc_version=$gcc_version"
