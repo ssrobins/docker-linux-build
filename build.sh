20c837b2de1f3f9cdc542951b1d4ca7b7b810a04
Steve Robinson <ssrobins@gmail.com>
2020-10-03 18:53:07 -0700

    Use env var to remove duplication of docker image name

--- build.sh
+++ build.sh
@@ -4,7 +4,8 @@ cd $(dirname "$0")
 
 gcc_version=9.3.0
 
-CI_REGISTRY_IMAGE=ghcr.io/ssrobins/docker-linux-build
+if [ -z "$DOCKER_IMAGE_NAME" ]; then
+    DOCKER_IMAGE_NAME=docker-linux-build
+fi
 
-
-docker build --pull --tag "$CI_REGISTRY_IMAGE:gcc$gcc_version" . --build-arg "gcc_version=$gcc_version"
+docker build --pull --tag "$DOCKER_IMAGE_NAME:gcc$gcc_version" . --build-arg "gcc_version=$gcc_version"
